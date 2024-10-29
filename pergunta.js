7d1e875ce00b03b19c00eae1ff00f5bdc18a4b54
estrela-caida <142031549+estrela-caida@users.noreply.github.com>
2024-10-29 08:38:53 -0300

    batata

---
+++ pergunta.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'História do cinema'
+    'Em que ano os irmãos Lumière apresentaram ao mundo o Cinematógrafo, e o que este aparelho fazia?'
+    '1895, o cinematógrafo projetava curtas-metragens em locais públicos'
+)
+criaCartao(
+'Teoria do cinema'
+'O que se aprende vendo filmes?'
+'Ver um filme é mais do que assistir, é experimentar, entender por dentro, destrinchar as linhagens históricas, as linguagens estéticas, filosóficas e culturais.'
+)
+criaCartao(
+    'Teoria da fotografia'
+    'Quais são os três piláres da fotografia?'
+    'luz, objeto e composição'
+)
+criaCartao(
+    
+)
+criaCartao(
+    
+)
+criaCartao(
+    
+)
+criaCartao(
+    
+)
+criaCartao(
+    
+)
+criaCartao(
+    
+)
+criaCartao(
+    
+)
+criaCartao(
+    
+)
+criaCartao(
+    
+)
